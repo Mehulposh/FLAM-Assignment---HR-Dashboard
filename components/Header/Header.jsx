213113f0e91f5f65448725bf933f5e14e69ff800
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2025-07-18 21:23:02 +0530

    added navbar in header file

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <nav className='bg-black text-white flex items-center  gap-10 p-4 font-semibold text-lg'>
+        <Link href='/' className='text-2xl'>HR Dashboard</Link>
+        <Link href='/bookmarks'>Bookmarks</Link>
+        <Link href='/analytics'>Analytics</Link>
+    </nav>
   )
 }
 
