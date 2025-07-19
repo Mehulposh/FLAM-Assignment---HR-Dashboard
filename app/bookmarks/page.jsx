63f0649c4cb96979b78df203f68a8354dd25d82b
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2025-07-19 15:49:39 +0530

    feat: enhance bookmarks page layout with improved styling and structure

--- app/bookmarks/page.jsx
+++ app/bookmarks/page.jsx
@@ -6,13 +6,15 @@ import React from 'react'
 const page = () => {
     const {bookmarks}  = useBookmarks();
   return (
-    <div>
-        <h1>Bookmarked Employees</h1>
-        {bookmarks.map(emp => (
-            <EmployeeCard employee={emp}/>
+    <div className='mt-3'>
+        <h1 className='text-center font-semibold text-3xl mb-4'>Bookmarked Employees</h1>
+        <div className='flex flex-wrap gap-4 p-3'>
+            {bookmarks.map(emp => (
+            <EmployeeCard key={emp.id} employee={emp}/>
         ))}
+        </div>
     </div>
   )
 }
 
-export default page
+export default page 
