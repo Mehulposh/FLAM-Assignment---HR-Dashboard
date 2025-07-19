ffe7dbad9977b431151ee5e2c71f372ec073aa04
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2025-07-19 16:32:03 +0530

    feat: update bookmarks page to conditionally render message for no bookmarked employees

--- app/bookmarks/page.jsx
+++ app/bookmarks/page.jsx
@@ -8,11 +8,17 @@ const page = () => {
   return (
     <div className='mt-3'>
         <h1 className='text-center font-semibold text-3xl mb-4'>Bookmarked Employees</h1>
-        <div className='flex flex-wrap gap-4 p-3'>
+        
+        {bookmarks.length > 0 ? (
+          <div className='flex flex-wrap gap-4 p-3'>
             {bookmarks.map(emp => (
             <EmployeeCard key={emp.id} employee={emp}/>
         ))}
         </div>
+        ) : (
+          <h1 className='text-xl font-bold'>No Bookmarked Employees</h1>
+        )}
+        
     </div>
   )
 }
--- components/Button/Button.jsx
+++ components/Button/Button.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Button = ({children,...props}) => {
   return (
     <button 
-        className='bg-blue-400 rounded p-1 m-1 text-gray-200 font-semibold'
+        className='bg-blue-400 rounded p-1 m-1 text-gray-200 font-semibold cursor-pointer'
         {...props}
       >
       {children}
